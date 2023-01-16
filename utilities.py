4a73a87369ddc2d008d723b9a7f12a7c9db07671
c4russian <martinmonzon00@gmail.com>
2023-01-16 12:54:48 -0300

    upt

---
+++ utilities.py
@@ -0,0 +1,47 @@
+def getUID():
+    filePATH='/etc/passwd'
+    separator=':'
+    def readFile(filePATH,separator):
+        f = open(filePATH ,'r')
+        fileList = f.readlines()
+        resultMatrix=[]
+        for i in range(len(fileList)):
+            matrix=fileList[i].split(separator)
+            resultMatrix.append(matrix)
+        f.close()   
+        return resultMatrix
+
+    resultMatrix=readFile(filePATH,separator)
+    UID_list = []
+    GID_list = []
+    for i in range(len(resultMatrix)):
+        UID_list.append(resultMatrix[i][2])
+        GID_list.append(resultMatrix[i][3])
+    UID_list = list(map(int, UID_list))
+    GID_list = list(map(int, GID_list))
+    UID=max(UID_list)+1
+    return UID
+
+def getGID():
+    filePATH='/etc/passwd'
+    separator=':'
+    def readFile(filePATH,separator):
+        f = open(filePATH ,'r')
+        fileList = f.readlines()
+        resultMatrix=[]
+        for i in range(len(fileList)):
+            matrix=fileList[i].split(separator)
+            resultMatrix.append(matrix)
+        f.close()   
+        return resultMatrix
+
+    resultMatrix=readFile(filePATH,separator)
+    UID_list = []
+    GID_list = []
+    for i in range(len(resultMatrix)):
+        UID_list.append(resultMatrix[i][2])
+        GID_list.append(resultMatrix[i][3])
+    UID_list = list(map(int, UID_list))
+    GID_list = list(map(int, GID_list))
+    GID=max(GID_list)+1
+    return GID
