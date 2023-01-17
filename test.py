e018688eef110d7cd6b65a49dd54ca2d80281c20
c4russian <martinmonzon00@gmail.com>
2023-01-17 16:48:59 -0300

    s

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
-from utilities import joinList
-from utilities import turnElementTostr
-ipadresses=[123,456,789]
-ipadresses=turnElementTostr(ipadresses)
-print(ipadresses)
-ipadresses=joinList(ipadresses,',')
-horario=['hentrada','hsalida']
-horario=joinList(horario,',')
-GECOS = ['fullname','workphone','homephone',horario]
-GECOS=joinList(GECOS,',')
-print(GECOS)
+import os
+# from utilities import joinList
+# from utilities import turnElementTostr
+# ipadresses=[123,456,789]
+# ipadresses=turnElementTostr(ipadresses)
+# print(ipadresses)
+# ipadresses=joinList(ipadresses,',')
+# horario=['hentrada','hsalida']
+# horario=joinList(horario,',')
+# GECOS = ['fullname','workphone','homephone',horario]
+# GECOS=joinList(GECOS,',')
+# print(GECOS)
+
+direccion='var/log/comandosDiarios.log'
+if os.path.exists('var/log')==False:
+    print("Directory doesn't exist.")
+
+cwd=os.getcwd()
+path=os.path.join(cwd,'var','log')
+print(path)
