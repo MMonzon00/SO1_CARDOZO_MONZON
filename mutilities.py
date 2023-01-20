1a3789b73098100859bbfd1ff4c4c4fce6de3531
erikacardozo <eri.carn088@gmail.com>
2023-01-20 19:35:09 -0300

    update propietario, ftp

--- mutilities.py
+++ mutilities.py
@@ -3,6 +3,19 @@ import datetime
 import os
 import time
 
+class colors:
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    WHITE = '\033[37m'
+    MAGENTA='\033[35m'
+    HEADER = '\033[95m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    
 def checkpath(path):
     return os.path.exists(path)
 def readFile(filePATH,separator):
@@ -120,6 +133,7 @@ def writePass(path,file):
 def joinList(list,separator):
     str=separator.join(list)
     return str
+
 
 if __name__== '__main__':
     print(days_since())
